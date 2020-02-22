2200f3b8a320f29bde58bfe0bcb100c86af9a409
Jordan M. Liebe <Jordan.Liebe@outlook.com>
2020-02-22 14:09:58 -0800

    Added Styling to Results Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import { fireEvent } from '@testing-library/react';
 
 class Response {
   Claim
   Analysis
   Articles = new Array()
+  ArticleCount
 };
+
+
 
 function App() {
   return (
@@ -24,14 +28,14 @@ function App() {
 
 function HomePage() {
   return (
-    <div id="Page">
+    <div id="HomePage">
       <div id="Float">
         <div id="Float-Title">Fake News Detection</div>
         <div id="Float-Query">
           <input name="Query"></input>
         </div>
         <div id="Float-Query">
-          <Link to="ResultsPage" ></Link>
+          <a href="./Results"><button>Analyze</button></a>
         </div>
       </div>
     </div>
@@ -43,15 +47,17 @@ function ResultsPage() {
   Test.Claim = "Are ghosts real?"
   Test.Analysis = "FALSE"
   Test.Articles = new Array("https://www.google.com", "https://yahoo.com");
+  Test.ArticleCount = 2;
 
   return (
-    <div id="Page">
-      <div id="Claim">{Test.Claim}</div>
-      <div id="Analysis">{Test.Analysis}</div>
-      <div id="Articles">
-        {
-          (Test.Articles).length
-        }
+    <div id="ResultsPage">
+      <div className="Card">
+        <div className="Card-Title">Claim</div>
+        <div className="Card-Content">"{Test.Claim}"</div>
+      </div>
+      <div className="Card">
+        <div className="Card-Title">Analysis</div>
+        <div className="Card-Content">{Test.Analysis}</div>
       </div>
     </div>
   );
